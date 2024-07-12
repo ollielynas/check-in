bacfd179571321c01dbda72832a9e5c38f232d25
Salamander <salamanderhash0514@gmail.com>
2024-07-12 21:05:40 +1200

    Start auth

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import sqlite3
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, redirect
+from argon2 import PasswordHasher
 
-conn = sqlite3.connect("db/db.sqlite")
+conn = sqlite3.connect("db/db.sqlite", check_same_thread=False)
 
 def setup_db():
     cur = conn.cursor()
@@ -23,10 +24,35 @@ def setup_db():
 
     conn.commit()
 
-setup_db()
+#setup_db()
 
 app = Flask(__name__)
 
-@app.route("/website/<path:path>", methods = ["GET"])
+@app.route("/website/<path:path>")
 def serve_file(path):
     return send_from_directory('website', path)
+
+@app.route("/")
+def redirect_to_index():
+    return redirect("/website/index.html")
+
+@app.route("/api/login", methods = ["POST"])
+def do_login():
+    print(request.form)
+    username = request.form["username"]
+    password = request.form["password"]
+    remeber = request.form["remember"] == "on"
+
+@app.route("/api/register", methods = ["POST"])
+def do_registration():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    hasher = PasswordHasher()
+    password_hash = hasher.hash(password)
+    
+    cur = conn.cursor()
+
+    res = cur.execute("SELECT id, username FROM user WHERE username = ?", [username])
+    if True or res.fethcone() is not None:
+        pass

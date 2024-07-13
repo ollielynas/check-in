97dd9fd1d73005dc334004c3ca9fb2067505e00d
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-07-13 12:20:18 +1200

    updated ui

--- website/main.js
+++ website/main.js
@@ -15,6 +15,7 @@ function button_press() {
     fetch("/api/button_press", {
         body: JSON.stringify({
             /* Put location here, "location": "Some location in a string format" */
+            location: navigator.location
         }),
         method: "POST"
     })
@@ -46,4 +47,42 @@ async function get_supervisees() {
 
 async function get_press_info(username) {
     return (await (await fetch(`/api/presses?username=${encodeURIComponent(username)}`)).json())["result"];
+}
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+function doSomething() {
+    var myCookie = getCookie("MyCookie");
+
+    if (myCookie == null) {
+        // do cookie doesn't exist stuff;
+    }
+    else {
+        // do cookie exists stuff
+    }
+}
+
+window.onload = () => {
+    if (getCookie("token") != null) {
+        document.querySelector(".sign-in-button").innerHTML = "<i class=\"ph ph-sign-out\"></i>";
+    }
 }

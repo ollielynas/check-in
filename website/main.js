a81a33a138b283de2959ae190b9c1768722a5c8f
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-07-13 15:53:06 +1200

    fixed cookie

--- website/main.js
+++ website/main.js
@@ -87,26 +87,14 @@ async function get_press_info(username) {
     return (await (await fetch(`/api/presses?username=${encodeURIComponent(username)}`)).json());
 }
 
-function getCookie(name) {
-    var dc = document.cookie;
-    var prefix = name + "=";
-    var begin = dc.indexOf("; " + prefix);
-    if (begin == -1) {
-        begin = dc.indexOf(prefix);
-        if (begin != 0) return null;
-    }
-    else
+function getCookie(name) 
     {
-        begin += 2;
-        var end = document.cookie.indexOf(";", begin);
-        if (end == -1) {
-        end = dc.length;
-        }
-    }
-    // because unescape has been deprecated, replaced with decodeURI
-    //return unescape(dc.substring(begin + prefix.length, end));
-    return decodeURI(dc.substring(begin + prefix.length, end));
-} 
+      const regex = new RegExp(`(^| )${name}=([^;]+)`)
+      const match = document.cookie.match(regex)
+      if (match) {
+        return match[2]
+      }
+   }
 
 function doSomething() {
     var myCookie = getCookie("MyCookie");
